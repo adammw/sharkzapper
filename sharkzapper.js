9039679fc43d830a9ee134a70b32676084071ca4
Adam Malcontenti-Wilson <adman.com@gmail.com>
2010-12-23 22:18:46 +1100

    bumping to version 1.3.4

--- sharkzapper.js
+++ sharkzapper.js
@@ -8,7 +8,7 @@
  */
 var recieveMessage, sendRequest, inject, tabnavListener;
 var debug = false;
-var thisVersion = '1.3.3';
+var thisVersion = '1.3.4';
 function inject_sharkzapper() {
     if(debug) console.log("injecting sharkzapper version "+thisVersion);
     sendRequest({"command": "getTabCount"});
